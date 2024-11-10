00c8e748f285ff2c54e33b6e4b1ff1c0d5e9f089
ThiagoKwon <thiago.v.s.kwon@gmail.com>
2024-11-10 17:24:48 -0300

    ajuste
    asasdssfd

--- codigo/TabelaHashFunc2.java
+++ codigo/TabelaHashFunc2.java
@@ -2,9 +2,7 @@ package codigo;
 
 public class TabelaHashFunc2 extends TabelaHash {
 
-	public int separador = -1;
-	public int diferencialImpar = -1;
-	public int diferencialPar = (getTamanho() / 2) + 1;
+	private static final int DIF = 21;
 
 	public TabelaHashFunc2(int tamanho) {
 		super(tamanho);
@@ -12,36 +10,14 @@ public class TabelaHashFunc2 extends TabelaHash {
 
 	@Override
 	public int funcaoHash(String nome) {
-		this.separador++;
-		this.diferencialImpar++;
-		if (this.diferencialImpar > getTamanho() / 2) {
-			this.diferencialImpar = 0;
-		}
-		this.diferencialPar++;
-		if (this.diferencialPar > getTamanho()) {
-			this.diferencialPar = 0;
-		}
-		int valorNome = 0;
+		int hash = 0;
+
 		for (int i = 0; i < nome.length(); i++) {
 			char letra = Character.toUpperCase(nome.charAt(i));
-			if (letra == '-') {
-				valorNome += 45;
-			} else {
-				int valorIndice = getIndiceAlfabeto(letra);
-				valorNome += valorIndice;
-			}
+			hash = DIF * hash + letra;
 		}
-		if (this.separador % 2 == 0) {
-			return this.diferencialPar;
-		} else {
-			return this.diferencialImpar;
-		}
+
+		return Math.abs(hash) % getTamanho();
 	}
 
-	private int getIndiceAlfabeto(char letra) {
-		if (letra >= 'A' && letra <= 'Z') {
-			return letra - 'A' + 1;
-		}
-		return 0;
-	}
 }
